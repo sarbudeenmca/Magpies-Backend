fab6c92a826eefea3c951067519f3666fcd9a51e
sarbudeen <sarbudeenmca@gmail.com>
2024-02-27 17:33:04 +0400

    Fake datas added for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder {
     /**
@@ -14,11 +15,28 @@ class DatabaseSeeder extends Seeder {
      */
     public function run() {
         // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory()->firstOrCreate([
+        //     'name' => 'Test User',
+        //     'email' => 'haja@exhibitv.tech',
+        //     'password' => Hash::make('admin'),
+        // ]);
 
-        \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'haja@exhibitv.tech',
-            'password' => Hash::make('admin'),
-        ]);
+        $faker = Faker::create();
+        for ($i = 0; $i < 600; $i++) {
+            DB::table('leads')->insert([
+                'lead_name' => $faker->name,
+                'company_name' => $faker->company,
+                'phone_number' => $faker->phoneNumber,
+                'mobile_number' => $faker->phoneNumber,
+                'email_address' => $faker->unique()->safeEmail,
+                'city' => $faker->city,
+                'country' => $faker->country,
+                'lead_status' => $faker->randomElement(['New', 'Potential', 'Confirmed', 'Dead', 'Followup', 'Revert']),
+                'lead_source' => $faker->randomElement(['Advertisement', 'Website', 'Referral', 'Social Media']),
+                'description' => $faker->text($MaxNbChars = 191),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
